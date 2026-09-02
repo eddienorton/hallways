c2d15b2b1ccb71493d04f1f4706e17753b0d0817
eddienorton <eddiebrayman@me.com>
2026-09-02 14:52:05 -0400

    Initial Commit

---
+++ Hallways/HallwaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HallwaysApp.swift
+//  Hallways
+//
+//  Created by Edward Brayman on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct HallwaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HallwaysTests/HallwaysTests.swift
@@ -0,0 +1,19 @@
+//
+//  HallwaysTests.swift
+//  HallwaysTests
+//
+//  Created by Edward Brayman on 9/2/26.
+//
+
+import Testing
+@testable import Hallways
+
+struct HallwaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
